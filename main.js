4bf74446f9bdcdd2add922578987e4d96326365b
Damian Galeano <dj.damian.b@hotmail.com>
2023-06-13 16:28:53 +0200

    In process

--- main.js
+++ main.js
@@ -1,52 +1,128 @@
 // Have the Main Menu and a Sides Menu
 
 var mainMenu = [
-    {dish: 'Pizza', price: 20},
-    {dish: 'Burger with bacon', price: 30},
-    {dish: 'Paella', price: 29}
+    { dish: 'Pizza', price: 20 },
+    { dish: 'Burger with bacon', price: 30 },
+    { dish: 'Paella', price: 29 }
 ];
 
 
 var accompanimentMenu = [
-    {dish: 'Potato cake', price: 9},
-    {dish:  'Salad', price: 4},
-    {dish: 'Prawns', price: 8}
-]
+    { dish: 'Potato cake', price: 9 },
+    { dish: 'Salad', price: 4 },
+    { dish: 'Prawns', price: 8 }
+];
 
 
-console.log(mainMenu);
+// COUSIN
 
-for(var i= 0; i < mainMenu.length; i++) {
-    var dish = mainMenu[i].dish;
-    var price = mainMenu[i].price;
-    console.log(dish + '' + price + ' €')
-}
+var breakfast = [
+    { dish: 'eggs', price: 2 },
+    { dish: 'bread', price: 2 },
+];
 
-for(var i= 0; i < accompanimentMenu.length; i++) {
-    var dish = accompanimentMenu[i].dish;
-    var price = accompanimentMenu[i].price;
-    console.log(dish + ' ' + price + ' -€')
-}
+var lunch = [
+    { dish: 'pasta', price: 2 },
+    { dish: 'meat', price: 3 },
+    { dish: 'rice', price: 2 },
+    { dish: 'soup', price: 2 },
+];
 
+
+var dinner = [
+    { dish: 'pasta', price: 4 },
+    { dish: 'meat', price: 5 },
+    { dish: 'rice', price: 3 },
+    { dish: 'soup', price: 3 },
+];
+
+
+
+// FOR MAINMENU & ACCOMPANIMENTMENU;
+
+// You get an entree and two side options at regular cost.
 
 function regularMenu() {
-    var menu1 = mainMenu[0].dish +' ' + accompanimentMenu[0].dish + ' ' + accompanimentMenu[1].dish;
+    var menu1 = mainMenu[0].dish + ' ' + accompanimentMenu[0].dish + ' ' + accompanimentMenu[1].dish;
     var price1 = mainMenu[0].price + accompanimentMenu[0].price + accompanimentMenu[1].price;
-    return `It is a big promo ${menu1}  just for ${price1}€`;
-}
-
-
-// - A user selects an entree.
-var userMainDish = mainMenu[2].dish;
-console.log(userMainDish)
-
-
-
-var waitressComment = 
+    return `It is a big promo  ${menu1}  just for ${price1}€`;
+};
 
 console.log(regularMenu())
 
 
-console.log(getMainMenuAndAccompaniment())
+// - show them the whole menu (print)
+
+console.log(mainMenu);
+console.log(accompanimentMenu);
+
+
+// - A user selects an entree.
+var userMainDish = mainMenu[2].dish;
+console.log(`i Wants for me ${userMainDish} with a little by ${accompanimentMenu[1].dish}`)
+
+
+// Waitress makes a comment based on their selection 
+
+var waitressComment = `We have different dishes I would recommend ${userMainDish} with a little of ${accompanimentMenu[1].dish}.`
+console.log(waitressComment)
+
+
+
+
+for (let property in mainMenu) {
+    if (mainMenu === property && accompanimentMenu === property) {
+        var there =
+            console.log(`${mainMenu.price} + ${accompanimentMenu.price}`)
+    }
+}
+
+// TELL THE PRICE
+
+
+for (var i = 0; i < mainMenu.length; i++) {
+    var dish = mainMenu[i].dish;
+    var price = mainMenu[i].price;
+    console.log(dish + '' + price + '€')
+}
+
+for (var i = 0; i < accompanimentMenu.length; i++) {
+    var dish = accompanimentMenu[i].dish;
+    var price = accompanimentMenu[i].price;
+    console.log(dish + ' ' + price + '€')
+}
+
+
+// - repeat the previous options for the side options (comment and price)
+
+console.log(Object.values(mainMenu))
+
+
+
+// FOR COUSIN
+// for BREAKFAST
+
+
+for (var i = 0; i < breakfast.length; i++) {
+    var dish = breakfast[i].dish;
+    var price = breakfast[i].price;
+    console.log('We have' + ' ' + dish + ' for ' + price + '€')
+}
+
+
+for (var i = 0; i < lunch.length; i++) {
+    var dish = lunch[i].dish;
+    var price = lunch[i].price;
+    console.log('We have' + ' ' + dish + ' for ' + price + '€')
+}
+
+
+
+
+for (var i = 0; i < dinner.length; i++) {
+    var dish = dinner[i].dish;
+    var price = dinner[i].price;
+    console.log('We have' + ' ' + dish + ' for ' + price + '€')
+}
 
 
